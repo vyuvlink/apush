2eb9174ea8d2c5e5184f49ec337ff1fb820bb353
vyuvlink <1063461427@qq.com>
2020-09-07 12:52:15 +0800

    push static

---
+++ myrat/build/precache-manifest.7e6f57de756a6e6738063fd1b42fe4db.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d48d3b80809ef477bfdbaa87f5175af",
+    "url": "/apush/index.html"
+  },
+  {
+    "revision": "17888000e788f61dac2b",
+    "url": "/apush/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "bc6b03bcbe265e1b6cd9",
+    "url": "/apush/static/js/2.5bf63f20.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/apush/static/js/2.5bf63f20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17888000e788f61dac2b",
+    "url": "/apush/static/js/main.d354b9c5.chunk.js"
+  },
+  {
+    "revision": "c697ab393cbfb7220990",
+    "url": "/apush/static/js/runtime-main.c49c1249.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/apush/static/media/logo.ee7cd8ed.svg"
+  }
+]);
